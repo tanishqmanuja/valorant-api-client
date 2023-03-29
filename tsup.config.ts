9ced101d88c9e3cbff6cfa1f858e12c463069585
tanishqmanuja <tanishqmanuja@gmail.com>
2023-03-29 17:06:14 +0530

    feat: named export providers from pkg

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/helpers/providers.ts"],
   format: ["esm", "cjs"],
   sourcemap: true,
   outDir: "dist",
