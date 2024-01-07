3492792200b22c0e0de4c263fdd8a116c2a44f97
tanishqmanuja <tanishqmanuja@gmail.com>
2024-01-07 14:36:23 +0530

    chore: change builder to unbuild

---
+++ build.config.ts
@@ -0,0 +1,11 @@
+import { resolve } from "path";
+import { defineBuildConfig } from "unbuild";
+
+export default defineBuildConfig({
+  entries: ["./src/index", "./src/types.ts", "./src/default-configs.ts"],
+  outDir: "dist",
+  declaration: true,
+  alias: {
+    "~": resolve(__dirname, "./src"),
+  },
+});
