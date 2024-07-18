4b60340cb434326f92be725a0934b314d277682a
tanishqmanuja <tanishqmanuja@gmail.com>
2024-07-18 15:05:47 +0530

    chore: update remote example

--- examples/vapic-remote.ts
+++ examples/vapic-remote.ts
@@ -1,8 +1,30 @@
-import { createValorantApiClient } from "@tqman/valorant-api-client";
+import {
+  createValorantApiClient,
+  isCloudflareError,
+} from "@tqman/valorant-api-client";
 import { presets } from "@tqman/valorant-api-client/presets";
 
-const RIOT_USERNAME = "YOUR_USERNAME";
-const RIOT_PASSWORD = "YOUR_PASSWORD";
+const RIOT_USERNAME = process.env.RIOT_USERNAME ?? "YOUR_USERNAME";
+const RIOT_PASSWORD = process.env.RIOT_PASSWORD ?? "YOUR_PASSWORD";
+
+if (!RIOT_USERNAME || !RIOT_PASSWORD) {
+  console.log("Please set RIOT_USERNAME and RIOT_PASSWORD");
+  process.exit(0);
+}
+
+console.log("Riot Username", RIOT_USERNAME);
+console.log(
+  "Riot Password",
+  RIOT_PASSWORD.slice(0, 3) + "*".repeat(RIOT_PASSWORD.length - 3),
+  "\n",
+);
+
+process.on("uncaughtException", error => {
+  if (isCloudflareError(error)) {
+    console.log("Blocked by Cloudflare");
+  }
+  process.exit(0);
+});
 
 const vapic = await createValorantApiClient(
   presets.remote.with({
