cce4a3644d2784ba91ba175df74894eb76e8cfb4
tanishqmanuja <tanishqmanuja@gmail.com>
2025-06-08 15:02:06 +0530

    chore: style release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -12,7 +12,12 @@ if (!version) {
   throw new Error("version is required");
 }
 
+console.log(`⬆️ Bump Version`);
 execSync(`pnpm version ${version} -m "chore: release %s"`, {
   stdio: "inherit",
 });
+process.stdout.write("\n");
+
+console.log(`🫸🏻 Push Tag`);
 execSync(`git push --follow-tags`, { stdio: "inherit" });
+process.stdout.write("\n");
